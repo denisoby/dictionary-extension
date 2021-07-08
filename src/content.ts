4469515d5bd8f228006f68d5736576cd224b4c9c
Nikita Ustimenko <ustimencko.nickita@yandex.ru>
2021-07-09 01:46:51 +0300

    fix: all the comments of the first review are taken into account

--- src/content.ts
+++ src/content.ts
@@ -5,10 +5,12 @@ if (document.location.href === 'https://en.wikipedia.org/wiki/Pug') {
 
 
 const CHECK_ONLY_SPACES_REGEXP = /^\s*$/
+const taggedWordElementClassName = 'tagged-word';
 
 function parsePage(node) {
     let textContent = "";
     const textNodes = {};
+    
     
     processNode(node);
 
@@ -41,42 +43,51 @@ const {textContent, textNodes}  = parsePage(document.body);
 console.log("All Text Nodes:", textContent, textNodes);
 
 function searchWord(textNode, nodeOffset, wordOffset, wordLength) {
-    debugger
-    const taggedWord = textNode.textContent.substr(nodeOffset, wordOffset); //получаем нужное слово
-    const endText = textNode.textContent.slice(nodeOffset+wordOffset); //все что после нужного слова
+    
+    const taggedWord = textNode.textContent.substr(wordOffset, wordLength); //получаем нужное слово
+    const endText = textNode.textContent.slice(wordOffset+wordLength); //все что после нужного слова
     const taggedWordElement = document.createElement("a");
 
     taggedWordElement.textContent = taggedWord;
-    taggedWordElement.className = 'tagged-word'; 
-    textNode.textContent = textNode.textContent.substr(0, nodeOffset);
+    taggedWordElement.className = taggedWordElementClassName; 
+    textNode.textContent = textNode.textContent.substr(0, wordOffset);
     textNode.after(taggedWordElement, endText);
     
-    textNodes[wordLength+textNode.length] = taggedWordElement.childNodes[0];
-    textNodes[wordLength+textNode.length+taggedWord.length] = taggedWordElement.nextElementSibling;
+    textNodes[nodeOffset+textNode.length] = taggedWordElement.childNodes[0];
+    textNodes[nodeOffset+textNode.length+taggedWord.length] = taggedWordElement.nextSibling;
 }
 
-searchWord(textNodes["290"], 7, 4, 290);
+searchWord(textNodes["290"], 290, 7, 4);
 console.log(textNodes["290"])
 
 
-function createTooltip(styleString) {
+function createTooltip(textTooltip) {
     const tooltipElement = document.createElement("a");
     tooltipElement.className = 'tooltip';
-    tooltipElement.textContent = "text";
-    tooltipElement.style = styleString;
-    document.body.append(tooltipElement);
+    tooltipElement.textContent = textTooltip;
+    
+    return tooltipElement
 }
 
-const tooltipElement = createTooltip(`
-        .tooltip {
-            position: 'absolute',
-            border: '1px solid #b3c9ce'
-        }`);
+const tooltipElement = createTooltip("temporary text");
+
+document.body.append(tooltipElement);
+
+const style = document.querySelector('style');
+
+function addStyle(styleString) {  
+    style.textContent = styleString;
+}
 
 document.addEventListener('mouseover', function(event) {
-
-    if(event.target.className === 'tagged-word') {
-        
+    if(event.target.classList.contains(taggedWordElementClassName)) {
+        addStyle(`
+            .tooltip{
+                position: absolute;
+                border : 1px solid #b3c9ce;
+                display: block;
+            }
+        `)
         let coords = event.target.getBoundingClientRect();
         let left = coords.left + (event.target.offsetWidth - tooltipElement.offsetWidth) / 2;
             if (left < 0) left = 0; // не заезжать за левый край окна
@@ -87,16 +98,15 @@ document.addEventListener('mouseover', function(event) {
         tooltipElement.style.left = coords.left + pageXOffset + 'px';
         tooltipElement.style.top = top + 'px';
         
-    }
-    
+    }    
 }, capture=true);
 
 document.addEventListener('mouseout', function() {
-
-    if (tooltipElement) {
-        
-    }
-
+    addStyle(`
+        .tooltip{
+            display: none;
+        }
+    `)
   }, capture=true);
 
 }
