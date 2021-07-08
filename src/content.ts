9b97690679370c7728877e039772868d1aa09d0c
Nikita Ustimenko <ustimencko.nickita@yandex.ru>
2021-07-08 19:15:23 +0300

    fix: change the name of the arguments of the searchWord function, extracted regexp

--- src/content.ts
+++ src/content.ts
@@ -2,83 +2,101 @@ if (document.location.href === 'https://en.wikipedia.org/wiki/Pug') {
     console.log('CONTENT SCRIPT: Hello World');
 
     // find('Pug', 'my love', document.body)
-}
 
-let allPageText = "";
-const textNodes = {};
-let tooltipElement
 
-function processNode(node) {
+const CHECK_ONLY_SPACES_REGEXP = /^\s*$/
+
+function parsePage(node) {
+    let textContent = "";
+    const textNodes = {};
     
-    if(node.nodeType === 3){
-        if(node.data.match(/^\s*$/)){
+    processNode(node);
+
+    return {
+        textContent,
+        textNodes
+    };
+
+    function processNode(node) {
+
+        if(node.nodeType === 3){
+            if(node.data.match(CHECK_ONLY_SPACES_REGEXP)){
+                return;
+            }
+            textNodes[textContent.length] = node;
+            textContent += node.data;
             return;
-        }
-        textNodes[allPageText.length] = node;
-        allPageText += node.data;
-        return;
-    } 
+        } 
+        
+        let childNodes = Array.from(node.childNodes || []);
     
-    let childNodes = Array.from(node.childNodes || []);
-
-    for(const childNode of childNodes) {
-        processNode(childNode)
+        for(const childNode of childNodes) {
+            processNode(childNode)
+        }
     }
 }
 
-processNode(document.body);
-console.log("All Text Nodes:",textNodes);
+const {textContent, textNodes}  = parsePage(document.body);
 
-function searchWord(textNode, startWord, lengthWord, offset) {
+console.log("All Text Nodes:", textContent, textNodes);
 
-    const taggedWord = textNode.textContent.slice(startWord, lengthWord+startWord); //получаем нужное слово
-    const endText = textNode.textContent.slice(lengthWord+startWord); //все что после нужного слова
+function searchWord(textNode, nodeOffset, wordOffset, wordLength) {
+    debugger
+    const taggedWord = textNode.textContent.substr(nodeOffset, wordOffset); //получаем нужное слово
+    const endText = textNode.textContent.slice(nodeOffset+wordOffset); //все что после нужного слова
     const taggedWordElement = document.createElement("a");
 
     taggedWordElement.textContent = taggedWord;
     taggedWordElement.className = 'tagged-word'; 
-    textNode.textContent = textNode.textContent.substr(0, startWord);
+    textNode.textContent = textNode.textContent.substr(0, nodeOffset);
     textNode.after(taggedWordElement, endText);
     
-    textNodes[offset+textNode.length] = taggedWordElement.childNodes[0];
-    textNodes[offset+textNode.length+taggedWord.length] = taggedWordElement.nextElementSibling;
+    textNodes[wordLength+textNode.length] = taggedWordElement.childNodes[0];
+    textNodes[wordLength+textNode.length+taggedWord.length] = taggedWordElement.nextElementSibling;
 }
 
-searchWord(textNodes["448"], 8, 4, 448);
-console.log(textNodes["448"])
+searchWord(textNodes["290"], 7, 4, 290);
+console.log(textNodes["290"])
 
 
+function createTooltip(styleString) {
+    const tooltipElement = document.createElement("a");
+    tooltipElement.className = 'tooltip';
+    tooltipElement.textContent = "text";
+    tooltipElement.style = styleString;
+    document.body.append(tooltipElement);
+}
 
+const tooltipElement = createTooltip(`
+        .tooltip {
+            position: 'absolute',
+            border: '1px solid #b3c9ce'
+        }`);
 
 document.addEventListener('mouseover', function(event) {
-    debugger
+
     if(event.target.className === 'tagged-word') {
-        tooltipElement = document.createElement("a");
-        tooltipElement.className = 'tooltip';
-        tooltipElement.textContent = "text";
-        tooltipElement.style.position = 'fixed';
-        tooltipElement.style.border = '1px solid #b3c9ce';
-        document.body.append(tooltipElement);
-
-
+        
         let coords = event.target.getBoundingClientRect();
         let left = coords.left + (event.target.offsetWidth - tooltipElement.offsetWidth) / 2;
-          if (left < 0) left = 0; // не заезжать за левый край окна
+            if (left < 0) left = 0; // не заезжать за левый край окна
         let top = coords.top - tooltipElement.offsetHeight - 5;
-        if (top < 0) { // если подсказка не помещается сверху, то отображать её снизу
+            if (top < 0) { // если подсказка не помещается сверху, то отображать её снизу
             top = coords.top + event.target.offsetHeight + 5;
-        }
+            }
         tooltipElement.style.left = coords.left + pageXOffset + 'px';
         tooltipElement.style.top = top + 'px';
+        
     }
     
-});
+}, capture=true);
 
 document.addEventListener('mouseout', function() {
 
     if (tooltipElement) {
-        tooltipElement.remove();
-        tooltipElement = null;
+        
     }
 
-  });
+  }, capture=true);
+
+}
